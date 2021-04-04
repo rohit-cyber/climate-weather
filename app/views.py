89ac981447de2ccc182b0f8980e308bb5534171e
rohit-cyber <rohitjune15@gmail.com>
2021-04-05 02:50:48 +0530

    connecting models with api

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse
+from .models import City
+from .forms import CityForm
 
 # Create your views here.
 
@@ -8,18 +10,32 @@ def index(request):
  
 
     url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=b528298732d4abd84d74b5a8aba55a13'
-    city='pune'
 
-    r=requests.get(url.format(city)).json()
+    if request.method=='POST':
+        form=CityForm(request.POST)
+        if form.is_valid():
+            form.save()
 
-    city_weather={
+    form=CityForm()
+
+    cities=City.objects.all()
+
+    weather_data=[]
+
+    for city in cities:
+
+        r=requests.get(url.format(city)).json()
+
+        city_weather={
         'city':r['name'],
         'temperature':r['main']['temp'],
         'description':r['weather'][0]['description'],
         'icon':r['weather'][0]['icon']
-    }
+        }
 
-    context={'city_weather':city_weather}
+        weather_data.append(city_weather)
+    print(weather_data)
+    context={'weather_data':weather_data,'form':form}
 
     return render(request,'weather.html',context)
 
